08528dc65eb7e36b53f17c52b757c6f379303b43
lucascampos04 <camposdlucasoli@gmail.com>
2023-10-15 12:17:32 -0300

    Alterações na tela home

--- src/main/java/com/cadastri/cadastro/controller/AlunoController.java
+++ src/main/java/com/cadastri/cadastro/controller/AlunoController.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
 
 @Controller
 public class AlunoController {
