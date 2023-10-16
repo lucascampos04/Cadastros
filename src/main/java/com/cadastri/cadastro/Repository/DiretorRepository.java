a6d537eaf1cc6654d130f1be0e29c37b51e0c222
lucascampos04 <camposdlucasoli@gmail.com>
2023-10-16 18:19:10 -0300

    Consultas de salvar e sekect dos dados funcionando

--- src/main/java/com/cadastri/cadastro/Repository/DiretorRepository.java
+++ src/main/java/com/cadastri/cadastro/Repository/DiretorRepository.java
@@ -1,11 +1,15 @@
 package com.cadastri.cadastro.Repository;
 
 import com.cadastri.cadastro.Model.Diretor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.core.CrudMethods;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiretorRepository extends CrudRepository<Diretor, String> {
+    Diretor findById(Long id);
+    boolean existsByRgOrCpf(String rg, String cpd);
 
 }
--- src/main/java/com/cadastri/cadastro/controller/FuncionarioController.java
+++ src/main/java/com/cadastri/cadastro/controller/FuncionarioController.java
@@ -1,11 +1,20 @@
 package com.cadastri.cadastro.controller;
 
+import com.cadastri.cadastro.Model.Diretor;
+import com.cadastri.cadastro.Repository.DiretorRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FuncionarioController {
+    @Autowired
+    private DiretorRepository dr;
+
     @RequestMapping("/selecionarProfissional")
     public String selecionarFuncionario(){
         return "Funcionarios/SelecionarFuncionario";
@@ -15,6 +24,33 @@ public class FuncionarioController {
     @RequestMapping(value = "/cadastrarDiretor", method = RequestMethod.GET)
     public String diretorForm(){
         return "Funcionarios/Diretor/formDiretor";
+    }
+
+    @RequestMapping(value = "/cadastrarDiretor", method = RequestMethod.POST)
+    public String diretorFormPost(@Valid Diretor diretor, BindingResult result, Model model){
+
+        if (diretor.getNomeCompleto().isEmpty() ||
+                diretor.getDataNascimento().isEmpty() ||
+                diretor.getGenero().isEmpty() ||
+                diretor.getRg().isEmpty() ||
+                diretor.getCpf().isEmpty() ||
+                diretor.getEndereco().isEmpty() ||
+                diretor.getEmail().isEmpty() ||
+                diretor.getFormacaoAcademica().isEmpty()) {
+
+            model.addAttribute("error", "Por favor, preencha todos os campos obrigatórios.");
+            System.out.println("Erro");
+            return "redirect:/cadastrarDiretor";
+        }
+
+        if (dr.existsByRgOrCpf(diretor.getRg(), diretor.getCpf())){
+            model.addAttribute("error", "Já existe um diretor com o mesmo RG ou CPF.");
+            System.out.println("PF e RG existentes");
+            return "redirect:/cadastrarDiretor";
+        }
+        dr.save(diretor);
+        System.out.println("FOI");
+        return "redirect:/cadastrarDiretor";
     }
 
 
